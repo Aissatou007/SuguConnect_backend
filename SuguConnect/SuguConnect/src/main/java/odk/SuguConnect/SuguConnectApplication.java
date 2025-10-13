6b1d5974f958d09aafabfc2438d673ea4a158f23
Aissatou Coulibaly <coulibalyaissatou37@gmail.com>
2025-10-13 14:50:02 +0200

    projet spring boot creer

---
+++ SuguConnect/SuguConnect/src/main/java/odk/SuguConnect/SuguConnectApplication.java
@@ -0,0 +1,13 @@
+package odk.SuguConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuguConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuguConnectApplication.class, args);
+	}
+
+}
---
+++ SuguConnect/SuguConnect/src/test/java/odk/SuguConnect/SuguConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package odk.SuguConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuguConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
